ec7c74bd13aacfd721bb8cbb9b481e375e6e3c28
Jared Forsyth <jared@jaredforsyth.com>
2010-03-20 20:11:40 -0600

    moved c tokens into a separate file

--- bnf/c/__init__.py
+++ bnf/c/__init__.py
@@ -5,8 +5,20 @@ import os
 
 lfile = lambda name:os.path.join(os.path.dirname(__file__),name)
 
-tokens = jbnf.Grammar(lfile('c.tokens.bnf'), extends = generic.tokens)
-tknames = tuple(x for x, in tokens.rules['<token>'])
-main = msbnf.Grammar(lfile('msdn_c.smaller.txt'))#, tokens = tknames)
+tokens = msbnf.Grammar(lfile('msdn_tokens.txt'))#, extends = generic.tokens)
+tknames = tuple(x for x, in tokens.rules['token'])
+# print tknames
+"""tknames = tuple(a.strip() for a in ''' identifier
+    keyword
+    comment
+    pp-directive
+    boolean-literal
+    integer-literal
+    real-literal
+    character-literal
+    string-literal
+    null-literal
+    operator-or-punctuator'''.split('\n') if a.strip())"""
+main = msbnf.Grammar(lfile('msdn_c.smaller.txt'), tokens = tknames)
 
 # vim: et sw=4 sts=4
