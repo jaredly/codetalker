5d002205927960b0a4ccb0ec0f3ba9ae9a69a91b
Jared Forsyth <jared@jaredforsyth.com>
2010-03-26 00:27:53 -0600

    mostly done w/ the antlr parser. tomorrow we'll see if it works

--- bnf/parsers/antlrbnf.py
+++ bnf/parsers/antlrbnf.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import string
 import grammar
 
 regex = '''
@@ -8,6 +9,45 @@ regex = '''
     (?<notchars>~\([^\)]+\))|
     (?<repeat>\*\+\?)
     '''
+
+def match(lchar,rchar,line,i,strings=True):
+    depth = 1
+    start = i
+    i += 1
+    instring = False
+    while depth > 0:
+        if instring:
+            if line[i] == "'":
+                instring = False
+            elif line[i] == "\\":
+                i += 1
+        elif line[i] == "'":
+            instring = True
+        elif line[i] == lchar:
+            depth += 1
+        elif line[i] == rchar:
+            depth -= 1
+        i += 1
+        if i >= len(line) and depth!=0:
+            print 'failed to match %s%s: %s' % (lchar,rchar,line)
+            fail
+    return start, i
+
+def match_string(i, line, char="'"):
+    start = i
+    i += 1
+    literal = ''
+    while line[i] != "'":
+        if line[i] == '\\':
+            literal += line[i:i+2]
+            i += 2
+        else:
+            literal += line[i]
+            i += 1
+        if i >= len(line):
+            print 'infinished string:',line
+            fail
+    return start,i+1
 
 def linehandle(line, rule, nsubs=0):
     items = [[]]
@@ -18,7 +58,7 @@ def linehandle(line, rule, nsubs=0):
         char = line[i]
         if char in ' \t':
             i += 1
-        elif char in '~*?':
+        elif char in '~*?+':
             items[-1].append(char)
             i += 1
         elif char == '.':
@@ -30,78 +70,106 @@ def linehandle(line, rule, nsubs=0):
                 i += 1
         elif char == "'":
             literal = '!'
-            i += 1
-            while line[i] != "'":
-                if line[i] == '\\':
-                    literal += line[i:i+2]
-                    i += 2
-                else:
-                    literal += line[i]
-                    i += 1
+            start, i = match_string(i, line)
+            literal += line[start+1:i-1]
             items[-1].append(literal)
             i += 1
         elif char == '(':
-            depth = 1
-            start = i
-            i += 1
-            instring = False
-            while depth > 0:
-                if instring:
-                    if line[i] == "'":
-                        instring = False
-                    elif line[i] == "\\":
-                        i += 1
-                elif line[i] == "'":
-                    instring = True
-                elif line[i] == '(':
-                    depth += 1
-                elif line[i] == ')':
-                    depth -= 1
-                i += 1
+            start, i = match('(',')',line,i,True)
+
             sitems, ssubs, nsubs = linehandle(line[start+1:i-1], rule, nsubs)
-            subs += ssubs
+            subs.update(ssubs)
             subs[rule + '-%d' % nsubs] = sitems
-            items[-1].append(rule + '-%d' % nsubs)
+            items[-1].append('@' + rule + '-%d' % nsubs)
             nsubs += 1
 
         elif char == '|':
             items.append([])
             i += 1
+
         elif char in string.ascii_letters:
             rulename = '@'
-            while line[i] in string.ascii_litters+string.digits+'-':
+            while i<len(line) and line[i] in string.ascii_letters+string.digits+'_':
                 rulename += line[i]
                 i += 1
             items[-1].append(rulename)
+
+        elif char == '{':
+            start, i = match('{','}',line,i,True)
+            print 'skipping:',line[start:i]
+
+        elif char == '=' and line[i+1] == '>':
+            print 'idk'
+            break
+
+        elif char == '/' and line[i+1] == '/':
+            break
+
         else:
             print 'weird char found: %s; %d; %s' % (line[i],i,line)
             fail
+    return items, subs, nsubs
 
 def split(text):
     rules = {}
+    num_lines = {}
     order = []
     name = None
+    startat = -1
+    items = []
+    lines = []
     inrule = False
-    for line in text.split('\n'):
+    for i,line in enumerate(text.split('\n')):
+        line = line.strip()
         if line.strip().startswith('//') or not line.strip():
             continue
         if inrule == 2 and line.strip() == ';':
+            num_lines[name] = [startat, lines]
+            items, rsubs, nsubs = linehandle(lines, name)
+            rules[name] = items
+            rules.update(rsubs)
+            for sname in rsubs:
+                num_lines[sname] = [startat, lines]
+            items = []
+            lines = ''
             inrule = 0
         elif not inrule:
-            inrule = 1
             name = line.split(' ')[0]
+            if name == 'fragment':
+                continue
+            inrule = 1
+            if line.split(' ',1)[-1].startswith(':'):
+                if line.endswith(';'):
+                    lines = line.split(':',1)[1][:-1]
+                    num_lines[name] = [startat, lines]
+                    items, rsubs, nsubs = linehandle(lines, name)
+                    rules[name] = items
+                    rules.update(rsubs)
+                    for sname in rsubs:
+                        num_lines[sname] = [startat, lines]
+                    items = []
+                    lines = ''
+                    inrule = 0
+            else:
+                order.append(name)
+                startat = i
         elif inrule == 1 and line.strip().startswith(':'):
             inrule = 2
-            rules[-1].append(line.strip()[1:].strip())
-        elif inrule == 2 and line.strip().startswith('|'):
-            rules[-1].append(line.strip()[1:].strip())
-    return rules
+            lines = line.strip()[1:]
+        elif inrule == 2:# and line.strip().startswith('|'):
+            if '//' in line:
+                line = line.split('//')[0]
+            lines += line.strip()
+    return rules, order, num_lines
 
 class Grammar(grammar.Grammar):
     def loadrules(self):
-        sections = split(self.original)
-        print sections[0]
-        print sections[-1]
-        print len(sections)
+        rules,order,lines = split(self.original)
+        self.rules = rules
+        self.order = order
+        self.lines = lines
+        print rules['HEX_LITERAL']
+        #print rules
+        print len(rules)
 
 # vim: et sw=4 sts=4
