5dd286a0aecfb542adfc5f626273d38d9ba581de
Jared Forsyth <jared@jaredforsyth.com>
2010-03-27 23:35:59 -0600

    added run_examples.sh

---
+++ run_examples.sh
@@ -0,0 +1,5 @@
+python examples/prettyjson.py test/json/one.json
+python examples/json.py test/json/one.json
+python examples/parse_c.py text/c/hanoi.c
+python examples/prettyc.py text/c/hanoi.c
+python examples/minify.py text/c/hanoi.c
