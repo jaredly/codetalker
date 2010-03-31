345f92872ed44bc81a0d3e869d1a41a83d2f5718
Jared Forsyth <jared@sage.aml.cs.byu.edu>
2010-03-31 17:52:35 -0600

    fixing examples

--- examples/prettyc.py
+++ examples/prettyc.py
@@ -14,36 +14,36 @@ def prettyfy(full):
     space = Node(' ',-1,True)
     lbreak = Node('\n',-1,True)
     tab = Node('    ',-1,True)
-    [n.appendChild(space) for n in full.getElementsByTagName('type')]
-    [n.appendChild(space) for n in full.getElementsByTagName('return-type') if n.children[0].name!='type']
-    [n.appendChild(space) for n in full.getElementsByTagName(',')]
+    [n.appendChild(space) for n in full.find('type')]
+    [n.appendChild(space) for n in full.find('return-type') if n.children[0].name!='type']
+    [n.appendChild(space) for n in full.find(',')]
 
-    [n.appendChild(lbreak) for n in full.getElementsByTagName('embedded-statement') if not n.children[0].name=='block']
-    [n.appendChild(lbreak) for n in full.getElementsByTagName('declaration-statement')]
-    [n.appendChild(lbreak) for n in full.getElementsByTagName('interface-member-declaration')]
-    for block in full.getElementsByTagName('block'):
+    [n.appendChild(lbreak) for n in full.find('embedded-statement') if not n.children[0].name=='block']
+    [n.appendChild(lbreak) for n in full.find('declaration-statement')]
+    [n.appendChild(lbreak) for n in full.find('interface-member-declaration')]
+    for block in full.find('block'):
         block.children.insert(1,lbreak)
         block.children.insert(0,space)
-        for exp in block.getElementsByTagName('comment'):
+        for exp in block.find('comment'):
             exp.children.insert(0,tab)
-        for exp in block.getElementsByTagName('declaration-statement'):
+        for exp in block.find('declaration-statement'):
             exp.children.insert(0,tab)
-        for exp in block.getElementsByTagName('embedded-statement'):
+        for exp in block.find('embedded-statement'):
             if exp.children[0].name == 'block':continue
             exp.children.insert(0,tab)
-        for exp in block.getElementsByTagName('block'):
+        for exp in block.find('block'):
             if exp is block:continue
             if len(exp.children)>2:
                 exp.children.insert(-1,tab)
 
-    for node in full.gETN('assignment-operator'):
+    for node in full.find('assignment-operator'):
         node.appendChild(space)
         node.children.insert(0,space)
-    for node in full.gETN('local-variable-declarator'):
+    for node in full.find('local-variable-declarator'):
         if len(node.children) == 3:
             node.children.insert(2,space)
             node.children.insert(1,space)
-    for node in full.gETN('comment'):
+    for node in full.find('comment'):
         node.children.insert(0,lbreak)
         node.appendChild(lbreak)
     
@@ -58,7 +58,7 @@ def prettyfy(full):
             'formal-p-right',
             'local-variable-declarator-list',
             'constant-declarator-tail'):
-        for node in full.getElementsByTagName(list_type):
+        for node in full.find(list_type):
             node.children.insert(1,space)
     print full
 
--- run_examples.sh
+++ run_examples.sh
@@ -1,5 +1,5 @@
 python examples/prettyjson.py test/json/one.json
 python examples/json.py test/json/one.json
-python examples/parse_c.py text/c/hanoi.c
-python examples/prettyc.py text/c/hanoi.c
-python examples/minify.py text/c/hanoi.c
+python examples/parse_c.py test/c/hanoi.c
+python examples/prettyc.py test/c/hanoi.c
+python examples/minify.py test/c/hanoi.c
