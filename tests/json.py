5b8e2a8d1e812202a8ad2b6b8537b232ee7a74f4
Jared Forsyth <jared@jaredforsyth.com>
2010-06-24 21:47:51 -0600

    moving some stuff around

--- tests/json.py
+++ tests/json.py
@@ -4,6 +4,8 @@ Testing for the codetalker.contrib.json grammar
 '''
 
 from magictest import MagicTest as TestCase, suite
-from codetalker.contrib.json import grammar, JSON
+# from codetalker.contrib.json import grammar, JSON
+
+all_tests = suite(__name__)
 
 # vim: et sw=4 sts=4
