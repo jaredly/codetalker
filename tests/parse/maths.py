420c0e24b34754cc2f3401d235226d0c404c4380
Jared Forsyth <jared@jaredforsyth.com>
2010-07-01 00:03:07 -0600

    test modifications

--- tests/parse/maths.py
+++ tests/parse/maths.py
@@ -5,8 +5,18 @@ from codetalker.pgm.grammar import TokenError
 
 import py.test
 
-def test_1():
+def _parse(text):
+    tree = grammar.process(text)
+    ss = str(tree)
+    assert ss == text
+    return tree
+
+def _test_1():
     assert 5 == m.from_string('2+3')
+
+def test_2():
+    tree = _parse('2+3')
+
 
 
 
--- tests/tokenize/maths.py
+++ tests/tokenize/maths.py
@@ -5,23 +5,27 @@ from codetalker.pgm.grammar import TokenError
 
 import py.test
 
+def _ttoken(text):
+    tk = grammar.get_tokens(text)
+    assert ''.join(token[-1] for token in tk) == text
+    return tk
+
 def test_one():
     py.test.raises(TokenError, grammar.get_tokens, '')
 
 def test_two():
-    tk = grammar.get_tokens('3')
+    tk = _ttoken('3')
 
 def test_three():
-    tk = grammar.get_tokens('+')
+    tk = _ttoken('+')
 
 def test_4():
-    tk = grammar.get_tokens('1 2 3 4 5')
+    tk = _ttoken('1 2 3 4 5')
 
 def test_5():
-    tk = grammar.get_tokens('()+-')
+    tk = _ttoken('()+-')
 
 def test_6():
-    tk = grammar.get_tokens('1 () 3.4+2')
-
+    tk = _ttoken('1 () 3.4+2')
 
 # vim: et sw=4 sts=4
