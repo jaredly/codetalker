a9b3b12dc4c9ab9ea32d5bb8572cd8fead59ab50
Jared Forsyth <jared@jaredforsyth.com>
2010-07-14 19:48:54 -0400

    fixing setup.py to work w/ sdist, test

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
-from setuptools import setup
+try:
+    from distutils.core import setup
+    from distutils.extension import Extension
+except ImportError:
+    print 'distutils is required to install this module. If you have pip installed, run: pip instal distutils'
+    raise
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    print 'Cython is required to install this module'
+    raise
+
 import os
+import glob
 
 try:
     fp = open(os.path.join(os.path.dirname(__file__), "README.rst"))
@@ -9,19 +21,38 @@ try:
 except IOError:
     readme_text = ''
 
+pyx_mods = []
+
+for filen in glob.glob('codetalker/pgm/cgrammar/*.pyx'):
+    if filen.split('/')[-1].startswith('_'):
+        continue
+    if filen.endswith('/tokens.pyx') or filen.endswith('/tokenize.pyx'):
+        extra = ['codetalker/pgm/cgrammar/_speed_tokens.c']
+    else:
+        extra = []
+    pyx_mods.append(Extension(filen.replace('/','.')[:-4], [filen]+extra))
+
+from test_cmd import test
+
 setup(
     name='CodeTalker',
     author='Jared Forsyth',
     author_email='jared@jaredforsyth.com',
-    version='0.3',
+    version='0.5',
     url='http://jaredforsyth.com/projects/codetalker/',
     download_url='http://github.com/jabapyth/codetalker/tree',
     description='a module for dynamic, pythonic language parsing',
     long_description=readme_text,
     classifiers=[
-        'Programming Language :: Python'
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
     ],
-    test_suite = 'tests.all_tests',
+    options={'test':{'test_dir':['tests/parse', 'tests/tokenize']}},
+
+    cmdclass = {'build_ext': build_ext , 'test':test},
+    ext_modules = pyx_mods,
+    include_dirs = 'codetalker',
+    packages = ['codetalker', 'codetalker.pgm', 'codetalker.contrib', 'codetalker.pgm.cgrammar'],
 )
 
 # vim: et sw=4 sts=4
