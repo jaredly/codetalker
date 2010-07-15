e007e69a9870d42175ff2cbfbfbe0f3393fd4a74
Jared Forsyth <jared@jaredforsyth.com>
2010-07-15 10:32:20 -0400

    removing old code

--- codetalker/__init__.py
+++ codetalker/__init__.py
@@ -1,15 +1,3 @@
 #!/usr/bin/env python
-import parser
-from node import Node
-import pgm
-
-def parse(text, lang, junk = ('whites','comment','pp-line'), debug=0):
-    root, i, const = parser.parse(text, lang.tokens)
-    tokens = list(node.children[0] for node in root.find('token'))
-    parser.debug = debug
-    root, i, const = parser.parse(tokens, lang.main, ignore=junk)
-    root.reparent()
-    root.finalize()
-    return root
 
 # vim: et sw=4 sts=4
--- codetalker/bnf/csharp/conv_bnf.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-text = open('c.bnf').read().replace('&lt;','<').replace('&gt;','>').replace('&amp;','&')
-lines = []
-for line in text.split('\n'):
-    line = 
-
-# vim: et sw=4 sts=4
