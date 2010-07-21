46a46a6cc5b205544943a0a254f6343984a75839
Jared Forsyth <jared@jaredforsyth.com>
2010-07-21 18:28:12 -0400

    optional tests are failing

--- tests/parse/optional.py
+++ tests/parse/optional.py
@@ -5,7 +5,24 @@ from codetalker.pgm.tokens import *
 from codetalker.pgm.special import *
 
 def start(rule):
-    rule | plus
+    rule | plus(value)
+
+def value(rule):
+    rule | ([ID], STRING)
+
+g = Grammar(start=start, tokens=[ID, STRING, WHITE], ignore=[WHITE])
+
+def test_one():
+    tree = g.get_ast('"string" "string" "strin" ')
+    assert len(tree) == 3
+
+def test_two():
+    tree = g.get_ast('one "two" three "four" five "six"')
+    assert len(tree) == 3
+
+def test_three():
+    tree = g.get_ast('"one" two "three" "four" five "six"')
+    assert len(tree) == 4
 
 
 # vim: et sw=4 sts=4
