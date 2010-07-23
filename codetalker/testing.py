f45bec46b611a02175ba3819869820502d131a65
Jared Forsyth <jared@jaredforsyth.com>
2010-07-23 18:34:00 -0400

    adding codetalker.testing, full of helpers for testing parsers

---
+++ codetalker/testing.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from codetalker.pgm.errors import ParseError
+
+def parse_rule(name, grammar):
+    parts = name.split('.')
+    mod = __import__(name, fromlist=['__dict__'])
+    def meta(rule, passing=(), failing=()):
+        def _pass(string):
+            def meta():
+                grammar.get_parse_tree(string, start=rule)
+            return meta
+        def _fail(string):
+            def meta():
+                try:
+                    res = grammar.get_parse_tree(string, start=rule)
+                except ParseError, e:
+                    pass
+                else:
+                    raise AssertionError('parsing was supposed to fail for', string, res)
+            return meta
+        for i, string in enumerate(passing):
+            fn = _pass(string)
+            mod.__dict__['test_pass_rule_%s #%d' % (rule.__name__, i)] = fn
+        for i, string in enumerate(failing):
+            fn = _fail(string)
+            mod.__dict__['test_fail_rule_%s #%d' % (rule.__name__, i)] = fn
+    return meta
+
+
+# vim: et sw=4 sts=4
--- tests/contrib/json.py
+++ tests/contrib/json.py
@@ -6,16 +6,38 @@ HERE = os.path.dirname(__file__)
 
 files = glob.glob(os.path.join(HERE, '../data/json/*.json'))
 
-from codetalker.contrib.json import loads
+from codetalker import testing
+import codetalker.contrib.json as json
+
+parse_rule = testing.parse_rule(__name__, json.grammar)
 
 def make_parse(fname):
     text = open(fname).read()
     def meta():
-        res = loads(text)
+        res = json.loads(text)
     return meta
 
 for fname in files:
     globals()['test_json "%s"' % fname] = make_parse(fname)
 
+parse_rule(json.dict_, (
+        '{}',
+        '{"one": 3}',
+        '{"one": 4, "two": "hi"}',
+        ), (
+        '{,}',
+        '{',
+        ))
+
+parse_rule(json.list_, (
+        '[]',
+        '[1,2]',
+        '[1,2,]',
+        '["hi"]',
+        '[1,"hi"]',
+        ), (
+        '[,]',
+        '[',
+        ))
 
 # vim: et sw=4 sts=4
